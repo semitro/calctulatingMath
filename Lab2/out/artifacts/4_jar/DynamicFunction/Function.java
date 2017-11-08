200ff22a9d8a89e2a6ae218f3ce76eb76653b692
semitro <semitro_8@mail.ru>
2017-11-08 19:01:59 +0300

    All the labs release

---
+++ Lab2/out/artifacts/4_jar/DynamicFunction/Function.java
@@ -0,0 +1,35 @@
+package DynamicFunction;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.sin;
+import static java.lang.Math.cos;
+import static java.lang.Math.abs;
+import static java.lang.Math.atan;
+import static java.lang.Math.asin;
+import static java.lang.Math.cosh;
+import static java.lang.Math.exp;
+import static java.lang.Math.log;
+import static java.lang.Math.log10;
+import static java.lang.Math.*;
+// Do not close the comment below!
+// This is the mark to change its code in runtime
+// Also we needed in 255 space symbol after the mark
+public class Function {
+
+    public static double f(double x) {
+        return
+                //__MAGIC__
+sqrt(x)//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                                                                                                                                                                                                                                   
+		;
+	}
+
+    public static double f_2(double x, double y){
+	return 
+  //__MAGIC__2
+y//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                                                                                                                                                                                                                                   
+		;
+	}
+}
